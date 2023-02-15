30d941052be4e22203ad5220d0b84c65d091088f
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-15 16:19:18 -0300

    feat: adicionando middleware de configuração do multer

--- src/router/index.router.ts
+++ src/router/index.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import cors from "cors";
 import controller from "../controller/index.controller";
+import upload from "../config/multer.config";
 
 const router = Router();
 
@@ -8,7 +9,7 @@ router.use(cors());
 
 router.delete("/delete/:id", controller.deleteExistingPost);
 router.patch("/update/:id", controller.updateExistingPost);
-router.post("/register", controller.registerNewPost);
+router.post("/register", upload.single("file"), controller.registerNewPost);
 router.get("/:id", controller.findPostBtId);
 router.get("/", controller.findAllPosts);
 

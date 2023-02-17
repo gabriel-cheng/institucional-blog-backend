70fa8a2b93886a04e823b9ef2bad06a51aa33c7b
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-17 15:36:21 -0300

    feat: criando rota de arquivos estáticos

--- src/router/index.router.ts
+++ src/router/index.router.ts
@@ -1,12 +1,13 @@
-import { Router } from "express";
+import express from "express";
 import cors from "cors";
 import controller from "../controller/index.controller";
 import upload from "../config/multer.config";
-
-const router = Router();
+import path from "path";
+const router = express.Router();
 
 router.use(cors());
 
+router.use("/files", express.static(path.resolve(__dirname, "../../" + "public/uploads")));
 router.delete("/delete/:id", controller.deleteExistingPost);
 router.patch("/update/:id", controller.updateExistingPost);
 router.post("/register", upload.single("file"), controller.registerNewPost);

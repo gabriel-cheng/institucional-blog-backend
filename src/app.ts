449d8c2451e3471816726638c159e06bf3fba52d
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-24 14:50:52 -0300

    chore: removendo linhas de código de criação de diretórios

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,9 @@ dotenv.config();
 import express from "express";
 import router from "./router/index.router";
 import MongoConnect from "./database/connect.database";
-import fs from "fs";
-const directoryExists = fs.existsSync("./public/uploads");
 
 MongoConnect();
 const app = express();
-
-if(!directoryExists) {
-    fs.mkdirSync("./public/uploads");
-}
 
 app.use(express.json());
 

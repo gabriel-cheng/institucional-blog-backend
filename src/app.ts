f0b218d8d3f244d1558a25543056c491bc4d7d73
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-28 13:53:03 -0300

    chore: corrição de rota de arquivos estáticos

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+const app = express();
 import router from "./router/index.router";
 import MongoConnect from "./database/connect.database";
+import path from "path";
 
 MongoConnect();
-const app = express();
+
+app.use("/posts/files", express.static(path.resolve(__dirname, "../" + "public/uploads")));
 
 app.use(express.json());
 
--- src/router/index.router.ts
+++ src/router/index.router.ts
@@ -2,12 +2,10 @@ import express from "express";
 import cors from "cors";
 import controller from "../controller/index.controller";
 import upload from "../config/multer.config";
-import path from "path";
 const router = express.Router();
 
 router.use(cors());
 
-router.use("/files", express.static(path.resolve(__dirname, "../../" + "public/uploads")));
 router.delete("/delete/:id", controller.deleteExistingPost);
 router.patch("/update/:id", upload.single("file"), controller.updateExistingPost);
 router.post("/register", upload.single("file"), controller.registerNewPost);
